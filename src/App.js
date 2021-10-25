8f4015aae88340d9ecec1839292b7821189add04
hanul-cha <ckgks3014@naver.com>
2021-10-25 21:10:29 +0900

    영화 api 가져오기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 import ReviewList from './components/ReviewList';
 
 function App() {
   return (
-    <div className="App">
-      <ReviewList />
-    </div>
+<BrowserRouter>
+      <div className="App">
+       {/* header */}
+       <Switch>
+        <Route exact path="/">
+          <ReviewList />
+        </Route>
+        <Route exact path="writereview">
+           
+        </Route>
+        
+       </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,8 +1,32 @@
 import React from "react";
 import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
+import axios from "axios";
 
 const ReviewList = () => {
+
+    const instance = axios.create({
+        baseURL: `http://kobis.or.kr/kobisopenapi/webservice/rest`
+    });
+
+    const getMoviesList = async() => {
+       try {
+        const response = await instance.get('boxoffice/searchDailyBoxOfficeList.json', {
+            params: {
+                key: '5b8f6908808242788ed34e31613a7fbe',
+                targetDt: '20210220'
+            }
+            
+        })
+        console.log(response)
+       } catch {
+
+       }
+    } 
+    
+    getMoviesList()
+
+
     return (
         <div>
             <Stack spacing={2}>
