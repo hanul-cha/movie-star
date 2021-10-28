f4f5f04d9922043cbfa4b5bb14d6247341e52a08
hanul-cha <ckgks3014@naver.com>
2021-10-28 16:53:35 +0900

    파일분리

---
+++ src/components/GetMoviesList.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "../env";
+
+export default function GetMoviesList() {
+    const [movies, setMovies] = useState()
+
+    useEffect(() => {
+        const MYKEY = process.env.REACT_APP_API_KEY;
+
+        const toDay = () => {
+            var date = new Date();
+            var year = date.getFullYear();
+            var month = ("0" + (1 + date.getMonth())).slice(-2);
+            var day = ("0" + (date.getDate() - 1)).slice(-2);
+            return year + month + day;
+        }
+    
+        const instance = axios.create({
+            baseURL: `http://kobis.or.kr/kobisopenapi/webservice/rest`
+        });
+    
+        const moviesList = async() => {
+           try {
+            await instance.get('boxoffice/searchDailyBoxOfficeList.json', {
+                params: {
+                    key: MYKEY,
+                    targetDt: `${toDay()}`
+                }
+                
+            })
+            .then(res => {
+                setMovies(res)
+            })
+            
+           } catch {
+    
+           }
+        }
+        moviesList()
+    },[])
+
+    
+
+    return movies
+}
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,39 +1,17 @@
 import React from "react";
 import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
-import axios from "axios";
-import "../env";
+import GetMoviesList from "./GetMoviesList";
 
 const ReviewList = () => {
-    const MYKEY = process.env.REACT_APP_API_KEY;
+    
+    const movieList = GetMoviesList()
 
-    const toDay = () => {
-        var date = new Date();
-        var year = date.getFullYear();
-        var month = ("0" + (1 + date.getMonth())).slice(-2);
-        var day = ("0" + (date.getDate() - 1)).slice(-2);
-        return year + month + day;
-    }
+    console.log(movieList)
+    
+    
 
-    const instance = axios.create({
-        baseURL: `http://kobis.or.kr/kobisopenapi/webservice/rest`
-    });
-
-    const getMoviesList = async() => {
-       try {
-        const response = await instance.get('boxoffice/searchDailyBoxOfficeList.json', {
-            params: {
-                key: MYKEY,
-                targetDt: `${toDay()}`
-            }
-            
-        })
-        console.log(response.data.boxOfficeResult.dailyBoxOfficeList)
-       } catch {
-
-       }
-    }
-    getMoviesList()
+   
     
 
 
