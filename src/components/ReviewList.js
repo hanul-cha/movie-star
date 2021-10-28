4b4aee84f6b6ef18994006b83bc3da89f74bd5bf
hanul-cha <ckgks3014@naver.com>
2021-10-28 18:38:41 +0900

    랭크 화면에 표시하기

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,33 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
 import GetMoviesList from "./GetMoviesList";
 
 const ReviewList = (props) => {
+    const [list, setList] =useState([])
+
+    useEffect(() => {
+        const movieList = async() => {
+            await GetMoviesList().then(res => {
+                setList(res.data.boxOfficeResult.dailyBoxOfficeList)
+            })
+        }
+        movieList();
+    },[])
     
-    const movieList = async() => {
+    
+    /* const movieList = async() => {
         await GetMoviesList().then(res => {
             return res
         })
-    }
-    
+    } */
 
-    
-    
-
-    console.log(movieList())
+    console.log(list)
+    //이렇게 하면 되긴 하는데... 이방법으로 리덕스 초기값에 넣을수 있을까??
    
     
 
 
     return (
+        <>
+        {list.map(movies => (
+            <div key={movies.rnum}>
+                <h2>{movies.movieNm}</h2>
+                <Stack spacing={2}>
+
+                    <Rating name="size-small" defaultValue={1} size="small" />
+                    
+                </Stack>
+            </div>
+        ))}
+
         <div>
-            <Stack spacing={2}>
-                <Rating name="size-small" defaultValue={1} size="small" />
-                <Rating name="size-medium" defaultValue={2} />
-                <Rating name="size-large" defaultValue={3} size="large" />
-            </Stack>
+            
         </div>
+        </>
     )
 }
 
